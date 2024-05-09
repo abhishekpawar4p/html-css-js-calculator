fac645cb1a6d7e58a6063db948d7e5a1d0101096
abhishekpawar4p <abhishekpawar4p@gmail.com>
2024-05-10 00:01:48 +0530

    Finished calculator Project

---
+++ app.js
@@ -0,0 +1,24 @@
+// calculator process
+
+const display = document.getElementById("display");
+
+function showToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = ""
+}
+
+
+function calculate(){
+
+    try{
+        display.value = eval(display.value)
+    }
+    catch(error){
+        display.value = "Error";
+    }
+    
+}
+
